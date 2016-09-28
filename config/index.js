aa5334fae1a68d5019d5fe7b047b97c253718a3c
Scott Wojan <scott.wojan@projekt202.com>
2016-09-28 15:04:02 -0500

    Adding enableSecurity property to config

--- config/index.js
+++ config/index.js
@@ -2,17 +2,20 @@
 
 let nconf = require('nconf');
 
+/*The environment, if none is set assume development*/
 exports.environment = process.env.NODE_ENV || 'development';
 
 nconf.argv().env().file({file: __dirname + '/' + exports.environment + '.json'});
 
-//Restify server settings
+/*Restify server settings*/
 exports.server = {
     name: process.env.SERVER_NAME || nconf.get('server:name'),
     version: process.env.SERVER_VERSION || nconf.get('server:version'),
-    port: process.env.SERVER_PORT || nconf.get('server:port')
+    port: process.env.SERVER_PORT || nconf.get('server:port'),
+    enableSecurity: process.env.SERVER_ENABLE_SECURITY || nconf.get('server:enableSecurity')
 };
 
+/*Database settings*/
 exports.database = {
     name: process.env.DATABASE_NAME || nconf.get('database:name'),
     host: process.env.DATABASE_HOST || nconf.get('database:host'),
